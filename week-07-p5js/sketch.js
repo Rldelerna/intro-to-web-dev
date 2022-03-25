6eb136902138405e3d9e3ab6cda5d93064034440
Rldelerna <98355940+Rldelerna@users.noreply.github.com>
2022-03-25 12:31:10 -0400

    Week 8 project

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -3,5 +3,35 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  background(255, 128, 0);
+
+  let circleX;
+let circleY;
+let circleSize;
+
+function setup() {
+  createCanvas(300, 300);
+  noFill();
+  strokeWeight(5);
+  circleX = width / 2;
+  circleY = height / 2;
+  circleSize = 0;
+  
+}
+
+function draw() {
+  background(0, 128, 255);
+
+  circleSize += 10;
+
+  stroke(0, 64, 128);
+  circle(circleX, circleY, circleSize);
+  circle(circleX, circleY, circleSize * .75);
+  circle(circleX, circleY, circleSize * .5);
+}
+
+function mousePressed(){
+  circleX = mouseX;
+  circleY = mouseY;
+  circleSize = 0;
 }
--- week-08-objects/script.js
+++ week-08-objects/script.js
@@ -1,26 +1,34 @@
 // Array of objects representing a todo list.
 // Modify this array to contain your own list.
 const taskArray = [
-  {label: 'Water plants', time: 3},
-  {label: 'Homework', time: 2},
-  {label: 'Laundry', time: 1},
+  {label: 'Breath of the Wild', time: 100, console: 'Switch'},
+  {label: 'Links Awakening', time: 30, console: 'Switch'},
+  {label: 'Phantom Hourglass', time: 20,console: 'DS'},
+  {label: 'Skyward Sword', time: 80, console: 'Wii'},
+  {label: 'Wind Waker', time: 50, console: 'Wii U'},
+  {label: 'Twilight Princess', time: 80, console: 'Wii'},
+  {label: 'Ocarina of Time', time: 50, console: 'Gamecube'},
+  {label: 'Age of Calamity', time: 70, console: 'Switch'},
+  {label: 'Majoras Mask', time: 20, console: 'Gamecube'},
+  {label: 'Spirit Tracks', time: 20, console: 'DS'},
 ];
 
 // Loads the content into the page.
 function loadContent() {
   // This line of code sorts the array alphabetically by the task labels.
   // Modify this to sort your data by a different field, or just delete it.
-  taskArray.sort((a, b) => compare(a.label, b.label));
+  taskArray.sort((a, b) => compare(a.console, b.console));
 
   loadTable();
-  loadShortestTask();
+  loadLongestTask();
 }
 
 // Adds a task to the array and reloads the page content.
 function addNewTask() {
   const newTaskLabel = document.getElementById('label-input').value;
   const newTaskTime = document.getElementById('time-input').value;
-  const newTask = {label: newTaskLabel, time: newTaskTime };
+  const newTaskConsole = document.getElementById('console-input').value;
+  const newTask = {label: newTaskLabel, time: newTaskTime, console: newTaskConsole};
   taskArray.push(newTask);
 
   loadContent();
@@ -32,9 +40,10 @@ function loadTable() {
 
   // Create a header row.
   const headerRowElement = document.createElement('tr');
-  headerRowElement.appendChild(createElement('th', 'Index'));
-  headerRowElement.appendChild(createElement('th', 'Label'));
-  headerRowElement.appendChild(createElement('th', 'Time'));
+  headerRowElement.appendChild(createElement('th', '#'));
+  headerRowElement.appendChild(createElement('th', 'Title'));
+  headerRowElement.appendChild(createElement('th', 'Hours Played'));
+  headerRowElement.appendChild(createElement('th', 'Console'));
   tableElement.appendChild(headerRowElement);
 
   // Iterate over the array and create a table row for each object.
@@ -44,6 +53,7 @@ function loadTable() {
     rowElement.appendChild(createElement('td', i));
     rowElement.appendChild(createElement('td', task.label));
     rowElement.appendChild(createElement('td', task.time));
+    rowElement.appendChild(createElement('td', task.console));
     tableElement.appendChild(rowElement);
   }
 
@@ -53,19 +63,19 @@ function loadTable() {
 }
 
 // Displays the name of the shortest task.
-function loadShortestTask(){
+function loadLongestTask(){
   // Assume the first task is shortest
-  let shortestTask = taskArray[0];
+  let longestTask = taskArray[0];
 
   // Starting with the second task, look for a shorter task
-  for (let i = 1; i < taskArray.length; i++) {
+  for (let i = 1; i > taskArray.length; i++) {
     const task = taskArray[i];
     // If this task is shorter than the previous shortest, it's now the shortest
-    if(task.time < shortestTask.time) {
-      shortestTask = task;
+    if(task.time > longestTask.time) {
+      longestTask = task;
     }
   }
-  document.getElementById('shortest-task').innerText = shortestTask.label;
+  document.getElementById('longest-task').innerText = longestTask.label;
 }
 
 // Helper function that creates an element that contains text content.
